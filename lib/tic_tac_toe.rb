08c3169c5efeee77d20aa31bdb75cb2e520abe4f
Ho Yin Keung <hykeung92@gmail.com>
2019-12-28 00:41:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,7 +90,15 @@ class  TicTacToe
   end
   
   def winner
-    
+    WIN_COMBINATIONS.detect do |combo| 
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" 
+        return "X"
+      elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        return "O"
+      else 
+        nil
+      end
+    end
   end
   
   
